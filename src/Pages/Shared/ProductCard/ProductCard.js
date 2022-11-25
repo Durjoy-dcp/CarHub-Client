729d64436db603b94c08c6ca6aa30ef53876e9ba
Durjoy-dcp <unknownccxv@gmail.com>
2022-11-25 15:49:30 +0600

    card almost final

--- src/Pages/Shared/ProductCard/ProductCard.js
+++ src/Pages/Shared/ProductCard/ProductCard.js
@@ -1,9 +1,10 @@
 import React, { useContext } from 'react';
-import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaMapMarkerAlt, FaCheckCircle } from "react-icons/fa";
+import Moment from 'react-moment';
 import { AuthContext } from '../../../context/AuthProvider';
 const ProductCard = ({ product, children }) => {
     const { currentYear } = useContext(AuthContext);
-    const { condition, date, name, location, img, orginalprice, price, sellername, year, _id, description, verifiedSeller } = product
+    const { condition, date, name, location, img, orginalprice, price, sellername, year, catagory, _id, description, verifiedSeller } = product
     const used_year = currentYear - year;
     return (
         <div className="card card-compact  bg-base-100 shadow-xl max-w-sm">
@@ -13,23 +14,34 @@ const ProductCard = ({ product, children }) => {
                 <p>{description.length > 60 ? description.slice(0, 60) + '...' : description}</p>
                 <div className='grid grid-cols-2 '>
                     <div className=' '>
-
                         <p className='text-xs'><FaMapMarkerAlt className=' mr-1 inline ' />{location}</p>
                     </div>
 
                     <div>
                         <p className='text-xs px-2'>{used_year === 0 ? '1 Year Running ' : used_year + 1 + ' years Running '}</p>
-
                     </div>
                 </div>
+                <div>
+                    <p className="text-xl font-bold">${price}
+                        <span className="text-xs font-bold bg-blue-500 mx-1 rounded text-white p-1"> Orginal : ${orginalprice} </span></p>
+                </div>
+                <div>
+                    <p>Condition : {condition}</p>
+                    <p >Catagory:<span className='bg-blue-500 text-white mx-1 p-1 text-xs rounded'>{catagory}</span> </p>
+                </div>
+                <div >
+                    <p className='gray-text'>{sellername}<span>{verifiedSeller && <FaCheckCircle className=" mx-2 text-blue-600 inline " />}</span> </p>
+                    <p className='text-xs text-end'>Posted <Moment fromNow>{date}</Moment></p>
+
+                </div>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+
                     {
                         children
                     }
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
