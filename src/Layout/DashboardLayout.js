c6375d305d0e61659c500e7cbdabf143598415c4
Durjoy-dcp <unknownccxv@gmail.com>
2022-12-21 20:21:09 +0600

    bug fix of h-full

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -19,9 +19,7 @@ const DashboardLayout = () => {
                     <NavHeaderDashboard></NavHeaderDashboard>
                     <Outlet></Outlet>
 
-                    <div className='relative h-full'>
-
-
+                    <div className=''>
                         <label tabIndex={0} className="btn fixed bg-slate-800  text-white top-1/2 left-0 w-16  lg:hidden" htmlFor="dashboardDrawer">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
