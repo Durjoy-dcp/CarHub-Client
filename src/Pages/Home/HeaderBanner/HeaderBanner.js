944082f5d18b39ef29bd69c20ee78f2810e3f763
Durjoy-dcp <unknownccxv@gmail.com>
2023-01-08 15:18:23 +0600

    animation updated

--- src/Pages/Home/HeaderBanner/HeaderBanner.js
+++ src/Pages/Home/HeaderBanner/HeaderBanner.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import headerpic from '../../../assets/header.png';
+import React from "react";
+import headerpic from "../../../assets/header.png";
+import bigCar from "../../../assets/bigCar.gif";
+import smokeWheel from "../../../assets/smoke.gif";
 const HeaderBanner = () => {
-    return (
-        <div className=' md:flex items-center my-2   '>
-
-
-            <div className=' md:order-last  md:w-1/2 p-3 container'>
-                <h1 className="lg:text-9xl text-center md:text-left  text-5xl my-3 bebus text-red-500  "> <span className='text-amber-500 '> Buy</span>  & Sell <br /> Your Car Here!</h1>
-                <p className='text-xl md:inline hidden  gray-text  '>Along with 1000s of cars to choose from, you can apply for finance online and value <br /> your existing car all from the comfort of your favourite armchair.</p>
-            </div>
-            <div>
-                <img src={headerpic} alt="" />
-            </div>
-            <div className='container md:hidden text-center p-6'>
-                <p className='text-xl inline   gray-text '>Along with 1000s of cars to choose from, you can apply for finance online and value <br /> your existing car all from the comfort of your favourite armchair</p>
-            </div>
-
-        </div>
-    );
+  return (
+    <div className=" md:flex items-center my-2   ">
+      <div className=" md:order-last  md:w-1/2 p-3 container mx-auto">
+        <h1 className="lg:text-9xl text-center md:text-left  text-5xl my-3 bebus text-red-500  ">
+          {" "}
+          <span className="text-amber-500 "> Buy</span> & Sell{" "}
+          <img src={smokeWheel} className="inline" alt="" /> <br /> Your Car
+          Here!
+        </h1>
+        <p className="text-xl md:inline hidden  gray-text  ">
+          Along with 1000s of cars to choose from, you can apply for finance
+          online and value <br /> your existing car all from the comfort of your
+          favourite armchair.
+        </p>
+      </div>
+      <div>
+        {/* <img src={headerpic} alt="" /> */}
+        <img src={bigCar} alt="" />
+      </div>
+      <div className="container md:hidden text-center p-6">
+        <p className="text-xl inline   gray-text ">
+          Along with 1000s of cars to choose from, you can apply for finance
+          online and value <br /> your existing car all from the comfort of your
+          favourite armchair
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default HeaderBanner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,41 +1,51 @@
-import React from 'react';
-import Footer from '../Footer/Footer';
-import Stat from '../Shared/Stat/Stat';
-import Advertisement from './Advertisement/Advertisement';
-import Brands from './Brands/Brands';
-import CatagoryShow from './CatagoryShow/CatagoryShow';
-import Feedback from './Feedback/Feedback';
-import HeaderBanner from './HeaderBanner/HeaderBanner';
-import './Home.css'
-import SecureBanner from './Securebanner/SecureBanner';
+import React from "react";
+import Footer from "../Footer/Footer";
+import Stat from "../Shared/Stat/Stat";
+import Advertisement from "./Advertisement/Advertisement";
+import Brands from "./Brands/Brands";
+import CatagoryShow from "./CatagoryShow/CatagoryShow";
+import Feedback from "./Feedback/Feedback";
+import HeaderBanner from "./HeaderBanner/HeaderBanner";
+import "./Home.css";
+import SecureBanner from "./Securebanner/SecureBanner";
+import car from "../../assets/car.gif";
 const Home = () => {
-    return (
+  return (
+    <div>
+      <HeaderBanner></HeaderBanner>
+      <div className="container   mx-auto">
         <div>
-            <HeaderBanner></HeaderBanner>
-            <div className='container   mx-auto'>
-
-                <h1 className="text-3xl   bebus p-3  " data-aos="fade-right">BROWSE BY CAR TYPE </h1>
-                <hr />
-            </div>
-            <div className='	'>
-
-                <CatagoryShow></CatagoryShow>
-                <Advertisement></Advertisement>
-                <SecureBanner></SecureBanner>
-            </div>
-            <div className='container   mx-auto'>
-
-                <h1 className="text-3xl   bebus p-3  " data-aos="fade-right">TOP brands of the month </h1>
-                <hr />
-            </div>
-            <Brands></Brands>
-            <Feedback></Feedback>
-            <div className='grid justify-items-center'>
-                <Stat className="my-5"></Stat>
-            </div>
-            <Footer></Footer>
+          <h1 className="text-3xl   bebus p-3  " data-aos="fade-right">
+            BROWSE BY CAR TYPE{" "}
+            <img
+              src={car}
+              className="inline-block align-middle"
+              style={{ width: "120px" }}
+              alt=""
+            />
+          </h1>{" "}
         </div>
-    );
+        <hr />
+      </div>
+      <div className="	">
+        <CatagoryShow></CatagoryShow>
+        <Advertisement></Advertisement>
+        <SecureBanner></SecureBanner>
+      </div>
+      <div className="container   mx-auto">
+        <h1 className="text-3xl   bebus p-3  " data-aos="fade-right">
+          TOP brands of the month{" "}
+        </h1>
+        <hr />
+      </div>
+      <Brands></Brands>
+      <Feedback></Feedback>
+      <div className="grid justify-items-center">
+        <Stat className="my-5"></Stat>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
